3b290f9c9814576bf9632c82276b1d9fde53a2f6
Jared Warren <jlwarren1@gmail.com>
2017-11-20 13:55:33 -0700

    testing

--- main.go
+++ main.go
@@ -3,35 +3,72 @@ package main
 // GOARM=6 GOARCH=arm GOOS=linux go build
 import (
 	"fmt"
+	"math/rand"
+	"path/filepath"
 
 	"gobot.io/x/gobot"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/raspi"
 )
 
+var episodes []string
+
 func main() {
 	r := raspi.NewAdaptor()
-	//led := gpio.NewLedDriver(r, "7")
 	button := gpio.NewButtonDriver(r, "17")
 
 	work := func() {
-		// gobot.Every(1*time.Second, func() {
-		// 	led.Toggle()
-		// })
-
 		button.On(gpio.ButtonPush, func(data interface{}) {
 			fmt.Println("PUSH")
 		})
 		button.On(gpio.ButtonRelease, func(data interface{}) {
-			fmt.Println("Release")
+			episode := getRandomEpisode()
+			playEpisode(episode)
 		})
 	}
 
-	robot := gobot.NewRobot("blinkBot",
+	robot := gobot.NewRobot("simpsons",
 		[]gobot.Connection{r},
 		[]gobot.Device{button},
 		work,
 	)
 
 	robot.Start()
+}
+
+func getRandomEpisode() string {
+	matchLen := len(episodes)
+	if matchLen == 0 {
+		episodes = getEpisodes("videos/Simpsons*")
+		matchLen = len(episodes)
+	}
+
+	i := rand.Intn(matchLen)
+	randomEpisode := episodes[i]
+	episodes = remove(episodes, i)
+	return randomEpisode
+}
+
+func getEpisodes(path string) []string {
+	matches, err := filepath.Glob(path)
+	if err != nil {
+		panic(err)
+	}
+	return matches
+}
+
+// remove this doesn't care about ordering
+func remove(s []string, i int) []string {
+	s[len(s)-1], s[i] = s[i], s[len(s)-1]
+	return s[:len(s)-1]
+}
+
+func playEpisode(episode string) {
+	if episode == "" {
+		return
+	}
+
+	fmt.Println("Play:", episode)
+	// kill process
+	// play video
 }
